fff01fa90a28ff315c690e72524e6f33c68483b2
FielX7 <angelocarltoledo@gmail.com>
2017-10-12 16:18:35 +0800

    Credentials Validation added

--- login.cpp
+++ login.cpp
@@ -15,7 +15,18 @@ LogIn::~LogIn()
 
 void LogIn::on_login_clicked()
 {
-    stackWidget->setCurrentIndex(2);
+    QString username = labLib->getUserId();
+    QString password = labLib->getPassword();
+    QString user_input = ui->user_input->text();
+    QString password_input = ui->password_input->text();
+
+    if(username == user_input && password == password_input){
+        ui->user_input->clear();
+        ui->password_input->clear();
+        stackWidget->setCurrentIndex(2);
+    } else {
+        labLib->showErrorMessageBox(false, "Error", "Username or password is incorrect. Please also check if caps lock is on.");
+    }
 }
 
 void LogIn::on_changepass_clicked()
